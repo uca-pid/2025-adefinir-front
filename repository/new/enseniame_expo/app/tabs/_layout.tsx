b424c8b6dc85275adbd82ac156b73da7d208096f
Belen-Eliza <beluella1@gmail.com>
2025-09-07 19:47:18 -0300

    estilo login

--- repository/new/enseniame_expo/app/tabs/_layout.tsx
+++ repository/new/enseniame_expo/app/tabs/_layout.tsx
@@ -5,7 +5,7 @@ import { Stack, Tabs } from "expo-router";
 export default function RootLayout() {
   return(
     <Tabs>
-    <Tabs.Screen name='index' options={{title:"Home",headerShown:false, tabBarIcon: ({focused,color})=><TabBarIcon name={focused? "home" : "home-outline"} color={color}/>}}/>
+    <Tabs.Screen name='index' options={{title:"Home", tabBarIcon: ({focused,color})=><TabBarIcon name={focused? "home" : "home-outline"} color={color}/>}}/>
     
     </Tabs>);
 }
