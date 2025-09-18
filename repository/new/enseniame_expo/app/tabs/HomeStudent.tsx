4373eea758a69e777a5262f3afb9b96519af4ee0
Belen-Eliza <beluella1@gmail.com>
2025-09-18 19:12:40 -0300

    pequeño error

--- repository/new/enseniame_expo/app/tabs/HomeStudent.tsx
+++ repository/new/enseniame_expo/app/tabs/HomeStudent.tsx
@@ -44,7 +44,7 @@ export default function HomeStudent() {
 
           {/* Atajos */}
           <View style={styles.shortcutsRow}>
-            <Pressable style={styles.shortcutCard} onPress={() => router.push('/tabs/cursos')}>
+            <Pressable style={styles.shortcutCard} onPress={() => router.push('/tabs/Cursos')}>
               <Ionicons name="book-outline" size={26} color="#560bad" />
               <Text style={styles.shortcutText}>Cursos</Text>
             </Pressable>
