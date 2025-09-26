26c267ab249d164a1c62fd4ae8300dfd21afdcf3
Belen-Eliza <beluella1@gmail.com>
2025-09-26 09:30:22 -0300

    nueva paleta de colores

---
+++ repository/new/enseniame_expo/components/colores.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const paleta_colores = StyleSheet.create({
+    aqua :{
+        backgroundColor: "#8bcac0",
+        color: "#8bcac0",
+    },
+    yellow: {
+        backgroundColor: "#ffbb51"
+    },
+    soft_yellow: {
+        backgroundColor: "#fdce85"
+    },
+    strong_yellow:{
+        backgroundColor: "#fcb64c"
+    },
+    softgray: {
+        backgroundColor: "#f6f6f6"
+    }
+})
